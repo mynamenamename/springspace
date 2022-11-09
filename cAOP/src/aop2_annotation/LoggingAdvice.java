c8743698b79b1b8a3726cb6bdd1c737973556388
Soyun Kim <soyun0661@gmail.com>
2022-11-09 18:05:55 +0900

    11.09 spring

---
+++ cAOP/src/aop2_annotation/LoggingAdvice.java
@@ -0,0 +1,31 @@
+package aop2_annotation;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(2)
+@Component   //bean 생성
+@Aspect      //advice 객체 생성 
+public class LoggingAdvice {
+
+	
+	@Before("execution(public * aop2_annotation.*.*Hello(..))")
+	public void before(JoinPoint point) throws Throwable
+	{
+	  String methodName = point.getSignature().getName();
+	  System.out.println("[********* 사전작업 ]" + methodName);
+	}
+	
+   @After("execution(public * aop2_annotation.*.*Hello(..))")
+	public void after(JoinPoint point) throws Throwable
+	{
+	   String methodName = point.getSignature().getName();
+       System.out.println("[********* 사후작업 ]" + methodName);
+	}
+	
+	
+}
